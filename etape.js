c24efe3de4d072b9d1dfc0e6ed373086d6d4a3d4
Pic <e1348790@cmaisonneuve.qc.ca>
2017-03-21 14:45:09 -0400

    installation package mongodb/ejs/express

--- etape.js
+++ etape.js
@@ -10,4 +10,27 @@ app.use(express.static('public'))  // pour utiliser le dossier public
 app.use(bodyParser.json())  // pour traiter les données JSON
 
 
+var db; // variable qui contiendra le lien sur la BD
 
+MongoClient.connect('mongodb://127.0.0.1:27017/version_etudiant', (err, database) => {
+  if (err) return console.log(err);
+  db = database;
+  app.listen(8081, () => {
+    console.log('connexion à la BD et on écoute sur le port 8081')
+  });
+});
+
+
+app.get('/',  (req, res) => {
+   console.log('la route route get / = ' + req.url);
+ 
+    var cursor = db.collection('adresse').find().toArray(function(err, resultat){
+       if (err) return console.log(err);
+    // renders index.ejs
+    // affiche le contenu de la BD
+    res.render('etape.ejs', {adresse: resultat});
+
+    });
+    
+
+});
