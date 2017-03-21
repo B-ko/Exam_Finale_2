6d627ad40bc1fe6196c60788b69cc8f352a08054
Pic <e1348790@cmaisonneuve.qc.ca>
2017-03-21 15:47:14 -0400

    etape 1

--- etape.js
+++ etape.js
@@ -9,28 +9,5 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'))  // pour utiliser le dossier public
 app.use(bodyParser.json())  // pour traiter les données JSON
 
-var obj;
-
-app.get('/', function (req, res) {
-
-	fs.readFile('public/text/collection_provinces.json', 'utf8', function (err, data) {
-		if (err) throw err;
-    		obj = JSON.parse(data);
-    		res.writeHead(200, {"Content-Type": "application/json"});
-			res.write(obj);
-			res.end();
-  	});
-
-  res.render('etape.js');
-
-})
-
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
-})
-
 
 
---
+++ serveur.js
@@ -0,0 +1,25 @@
+var http = require("http");
+var express = require('express');
+var app = express();
+var fs = require("fs");
+var obj;
+
+
+fs.readFile('public/text/collection_provinces.json', 'utf-8', function (err, data) {
+		if (err) return console.error(err);
+		obj = JSON.stringify(data)
+});
+
+app.get('/', function (req, res) {
+		  res.writeHead(200, {"Content-Type": "application/json"});
+		  res.write(obj);
+		  res.end();
+		})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+   console.log('Server running.');
+})
